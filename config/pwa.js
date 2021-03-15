45f2b8ba717c923908151ee6473dea9ca3993369
Michiel Koning <michiel.koning@valtech.com>
2021-03-15 19:45:16 +0100

    bg color

--- config/pwa.js
+++ config/pwa.js
@@ -3,20 +3,21 @@ import { title, categories, twitter, themeColor } from './../data/details'
 // PWA module configuration: https://go.nuxtjs.dev/pwa
 export default {
   manifest: {
+    background_color: themeColor,
+    categories,
     lang: 'nl',
     name: title,
-    shortName: title,
-    categories,
     orientation: 'portrait-primary',
+    shortName: title,
   },
   meta: {
-    twitterSite: twitter,
-    twitterCreator: twitter,
-    twitterCard: 'summary_large_image',
-    theme_color: themeColor,
+    appleStatusBarStyle: 'black-translucent',
     mobileApp: true,
     mobileAppIOS: true,
-    appleStatusBarStyle: 'black-translucent',
+    theme_color: themeColor,
+    twitterCard: 'summary_large_image',
+    twitterCreator: twitter,
+    twitterSite: twitter,
     viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
   },
 }
--- data/details.js
+++ data/details.js
@@ -1,7 +1,7 @@
-export const title = 'Cancel Qatar'
+export const apiUrl = 'https://api.cancelqatar.nl/graphql'
+export const baseUrl = 'https://cancelqatar.netlify.app'
 export const categories = ['politics', 'sports']
-export const twitter = '@CancelQatar2022'
 export const facebook = 'https://www.facebook.com/CancelWKQatar2022'
 export const themeColor = '#333'
-export const baseUrl = 'https://cancelqatar.netlify.app'
-export const apiUrl = 'https://api.cancelqatar.nl/graphql'
+export const title = 'Cancel Qatar'
+export const twitter = '@CancelQatar2022'
