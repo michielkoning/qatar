f0716eb2a84d1198cb26712cb1cbf79610a79ea3
Michiel Koning <michiel.koning@valtech.com>
2021-03-19 13:17:49 +0100

    color

--- data/details.js
+++ data/details.js
@@ -2,6 +2,6 @@ export const apiUrl = 'https://api.cancelqatar.nl/graphql'
 export const baseUrl = 'https://cancelqatar.nl'
 export const categories = ['politics', 'sports']
 export const facebook = 'https://www.facebook.com/CancelWKQatar2022'
-export const themeColor = '#333'
+export const themeColor = '#891a3b'
 export const title = 'Cancel Qatar'
 export const twitter = '@CancelQatar2022'
