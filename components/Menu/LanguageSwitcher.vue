a8512d9e86513f053b5c22a9a5b1cb47b96134d5
Michiel Koning <mail@michielkoning.nl>
2021-11-02 08:38:40 +0100

    talen

---
+++ components/Menu/LanguageSwitcher.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <nuxt-link
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      :class="$style.link"
+      :to="generateLocalePath(locale.code)"
+      @click.native="changePage"
+    >
+      <app-icon :icon="`flag-${locale.code}`" :title="locale.name" />
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+import { replacePlaceholder } from '~/helpers/i18n'
+import EventBusUtil from '~/utils/eventBusUtil'
+
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    },
+  },
+  methods: {
+    generateLocalePath(code) {
+      return this.switchLocalePath(code).replace(replacePlaceholder, '')
+    },
+    changePage() {
+      if (!this.isSmallScreen) return
+      EventBusUtil.$emit('change-page')
+    },
+    isSmallScreen() {
+      return window.innerWidth < 768
+    },
+  },
+}
+</script>
+
+<style lang="postcss" module>
+.link {
+  @mixin link-reset;
+
+  margin-bottom: var(--spacing-xxs);
+  display: flex;
+  align-items: center;
+
+  @media (--navigation-md) {
+    display: inline-block;
+    margin: 0 var(--spacing-xxs);
+  }
+}
+
+.title {
+  box-shadow: 0 1px 0 0 currentColor;
+  margin-left: var(--spacing-xs);
+
+  @nest .link:hover &, .link:focus & {
+    box-shadow: 0 2px 0 0 currentColor;
+  }
+
+  @media (--navigation-md) {
+    @mixin sr-only;
+  }
+}
+</style>
